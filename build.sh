09caca273872eb3fea82efb5bbb8ad1f36ca97c8
Martin Wimpress <code@flexion.org>
2009-09-20 18:48:04 +0100

    Updated release script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 function build {
     RELEASE_NAME="DVD-to-MPG"
     RELEASE_VER="1.0"
+    RELEASE_DESC="Rips a DVD to MPEG2-PS preserving chapters and subtitles."
+    RELEASE_KEYWORDS="DVD, rip, ripper, Linux, script, chapters, requant, subtitles, DVD5, MPEG2"
 
     rm ${RELEASE_NAME}-v${RELEASE_VER}.tar* 2>/dev/null
     bzr export ${RELEASE_NAME}-v${RELEASE_VER}.tar
